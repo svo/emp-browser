a0e0ed141f04c85d97ecaae691de585a2403f633
Sean Van Osselaer <svanosselaer@aconex.com>
2017-09-13 08:19:20 +1000

    Adding react-bootstrap for UI elements

--- src/App.js
+++ src/App.js
@@ -1,19 +1,18 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Jumbotron } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap-theme.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+      <Jumbotron>
+        <div className='container'>
+          <h1>EMP</h1>
+          <p>Create your payslips conveniently</p>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      </Jumbotron>
     );
   }
 }
