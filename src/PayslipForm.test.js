35691f114c73ef3e42f0bcd2c13154117c18f8a6
Sean Van Osselaer <svanosselaer@aconex.com>
2017-09-15 08:59:25 +1000

    Ensuring year and month are set as expected

--- src/PayslipForm.test.js
+++ src/PayslipForm.test.js
@@ -5,7 +5,25 @@ import renderer from 'react-test-renderer';
 
 it('renders correctly', () => {
   const tree = renderer.create(
-    <PayslipForm year={2017} month={3} />
+    <PayslipForm year={2017} month={1} />
   ).toJSON();
   expect(tree).toMatchSnapshot();
+});
+
+it('should have correct year property', () => {
+  const div = document.createElement('div');
+  var payslipForm = ReactDOM.render(<PayslipForm year={1976} month={3} />, div);
+  expect(payslipForm.props.year).toEqual(1976);
+});
+
+it('should have correct month property', () => {
+  const div = document.createElement('div');
+  var payslipForm = ReactDOM.render(<PayslipForm year={1976} month={3} />, div);
+  expect(payslipForm.props.month).toEqual(3);
+});
+
+it('should have correct month state', () => {
+  const div = document.createElement('div');
+  var payslipForm = ReactDOM.render(<PayslipForm year={1976} month={3} />, div);
+  expect(payslipForm.state.month).toEqual("APRIL");
 });
