fe58d134be1bba565966f54ec5abed4379037751
Sean Van Osselaer <svanosselaer@aconex.com>
2017-09-20 09:07:14 +1000

    Working at getting POST interaction working with local 'emp' docker container

--- src/actions/PayslipActions.js
+++ src/actions/PayslipActions.js
@@ -1,7 +1,25 @@
+import axios from 'axios';
+import names from 'date-names';
 import alt from '../alt';
 
 class PayslipActions {
+  createPayload(details) {
+    return {first_name: details.first_name,
+      last_name: details.last_name,
+      annual_salary: parseInt(details.annual_salary, 10),
+      year: details.month.getFullYear(),
+      month: names.months[details.month.getMonth()].toUpperCase(),
+      super_rate: parseFloat(details.super_rate)}
+  }
+
   create(details) {
+    axios.post('/payslip', this.createPayload(details))
+      .then(function (response) {
+        console.log(response);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
     return "/";
   }
 }
--- src/actions/PayslipActions.test.js
+++ src/actions/PayslipActions.test.js
@@ -1,5 +1,34 @@
 import PayslipActions from './PayslipActions';
 
+it('should create payload for create', () => {
+  var details = {first_name: "Sean",
+    last_name: "Van Osselaer",
+    annual_salary: 175000,
+    month: new Date('2017-04'),
+    super_rate: 9};
+
+  expect(PayslipActions.createPayload(details)).toEqual({
+    "first_name": "Sean",
+    "last_name": "Van Osselaer",
+    "annual_salary": 175000,
+    "year": 2017,
+    "month": "APRIL",
+    "super_rate": 9
+  });
+})
+
 it('should create', () => {
-  expect(PayslipActions.create({coconuts: true})).toEqual("/");
+  var details = {
+    "first_name": "Sean",
+    "last_name": "Van Osselaer",
+    "annual_salary": 175000,
+    "year": 2017,
+    "month": "APRIL",
+    "super_rate": 9
+  }
+
+  const createPayload = jest.fn();
+  createPayload.mockReturnValue(details)
+  PayslipActions.createPayload = createPayload;
+  expect(PayslipActions.create(details)).toEqual("/");
 });
